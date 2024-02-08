96e32cbfca4474e726ed95fbb57df5ad219738f8
krishnakp17 <krishnakp4765@gmail.com>
2024-02-09 00:49:15 +0530

    searching patient

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPatient from './components/AddPatient';
 import ViewPatient from './components/ViewPatient';
+import SearchPatient from './components/SearchPatient';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<AddPatient/>}/>
         <Route path='/view' element={<ViewPatient/>}/>
+        <Route path='/search' element={<SearchPatient/>}/>
       </Routes>
       </BrowserRouter>
       
